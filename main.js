4fb71492fe72d96e1ce8b7fa68d5c36c47c44c49
Mark Doten <mark.doten@gmail.com>
2016-04-19 15:52:30 -0700

    Adding main file for pulling in all json files

---
+++ main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  CARD: require('./nls/card-component.json'),
+  CAROUSEL: require('./nls/carousel-component.json'),
+  COMMENTS: require('./nls/comments.json'),
+  MAP: require('./nls/map-component.json'),
+  MEDIA_UPLOAD: require('./nls/media-upload.json'),
+  MOSAIC: require('./nls/mosaic-component.json'),
+  STREAMHUB_WALL: require('./nls/streamhub-wall.json')
+};
