2eda46edf2960ef3d0baa716ebd5882beed93e4d
Kyle Taborski <ktaborski@livefyre.com>
2016-04-19 21:09:10 -0700

    LF-22008 adding language codes for browsers

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ module.exports = {
   CARD: require('./nls/card-component.json'),
   CAROUSEL: require('./nls/carousel-component.json'),
   COMMENTS: require('./nls/comments.json'),
+  LANGUAGE_CODES: require('./nls/language-codes.json'),
   MAP: require('./nls/map-component.json'),
   MEDIA_UPLOAD: require('./nls/media-upload.json'),
   MOSAIC: require('./nls/mosaic-component.json'),
