dcd437fdd273fa42afef1a3609c8c85c9f32cafd
Mark Doten <mark.doten@gmail.com>
2016-05-03 08:49:52 -0700

    LF-22074: Adding reviews translations
    https://livefyre.atlassian.net/browse/LF-22074

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ module.exports = {
     MAP: require('./nls/map-component.json'),
     MEDIA_UPLOAD: require('./nls/media-upload.json'),
     MOSAIC: require('./nls/mosaic-component.json'),
+    REVIEW: require('./nls/review-component.json'),
     STREAMHUB_WALL: require('./nls/streamhub-wall.json')
   },
   LANGUAGE_CODES: require('./language-codes.json')
